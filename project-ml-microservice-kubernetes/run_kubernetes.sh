f8da192230cdaebe4c57f7028d360012128cb24e
Youssef Bayouli <bayouliyoussef@gmail.com>
2021-06-12 20:11:07 +0100

    making some changes on run kube file

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -4,15 +4,17 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
-
+dockerpath=api
 # Step 2
 # Run the Docker Hub container with kubernetes
-
-
+kubectl run api --image=$dockerpath --port=80
+echo 'Step2 done'
 # Step 3:
 # List kubernetes pods
-
+#kubectl wait --for=condition=ready pod --all
+kubectl get pods
+echo 'step 3 done'
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward api 8000:80
+echo 'step 4 done
