c8bcfb83541a5658668fffefa9370cf6c6d1c11e
Youssef Bayouli <bayouliyoussef@gmail.com>
2021-06-13 13:29:17 +0100

    changing docker path

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -4,7 +4,7 @@
 
 # Step 1:
 # This is your Docker ID/path
-dockerpath=api
+dockerpath="spacegeekops/api"
 # Step 2
 # Run the Docker Hub container with kubernetes
 kubectl run api --image=$dockerpath --port=80
